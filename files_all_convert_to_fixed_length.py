ed532adbb706a99619d1b428cf6e37f686daae26
aert <spam.aert@gmail.com>
2012-09-19 19:59:31 +0200

    Initial commit.

---
+++ files_all_convert_to_fixed_length.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+def main():
+    tailleFixe = 80
+    ficEntree = '<HERE-INPUT-FILE-PATH>'
+    ficSortie = '<HERE-OUTPUT-FILE-PATH>'
+
+    numLigne = 0
+    with open(ficEntree, "r") as fEntree, \
+         open(ficSortie, "w") as fSortie:
+
+        for line in fEntree:
+            lineIn = line.rstrip('\r\n')
+
+            numLigne = numLigne + 1
+            taille = len(lineIn)
+            if taille < tailleFixe:
+                nbBlanks = tailleFixe - taille
+                print "blanks added, old: %s - added: %s" % (taille, nbBlanks)
+                strBlanks = " " * nbBlanks
+                fSortie.write(lineIn + strBlanks + "\n")
+            elif taille > tailleFixe:
+                print "current width > fixed width, at line %s" % numLigne
+
+
+if __name__ == '__main__':
+    main()
